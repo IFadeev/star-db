854d5cd5abaec8f14b4215014cd0260ddb427689
Fadeev Ivan <githubfadeev@gmail.com>
2019-10-05 12:03:00 +0300

    Created SwapiService API class for access to swapi.co API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-const getResource = async (url) => {
-  const res = await fetch(url)
+
+class SwapiService {
+  
+  _apiBase = 'https://swapi.co/api';
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url} ${res.status}`)
+    }
+    return await res.json();
+  }
+
+  async getPeopleAll() {
+    const res = await this.getResource(`/people/`);
+    return res.results
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`);
+  }
+
 }
+
+const swapi = new SwapiService();
+
+swapi.getPerson(4).then(p => {
+  console.log(p.name )
+})
+
+// getResource('https://swapi.co/api/people/101010144444/').then( (body) => {
+//   console.log(body);
+// })
+//   .catch( (err) =>{
+//     console.error('ee', err)
+//   })
